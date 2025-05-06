49dd8c2989a9bc8cbb0eea6de465b7d93cf76ad0
Tevyn <tevyn.payne@gmail.com>
2025-05-06 15:22:09 -0500

    associating the right and wrong buttons with their functions and displaying green or red if right or wrong. Displaying a final message when the quiz is complete with final score

--- ui.py
+++ ui.py
@@ -10,7 +10,7 @@ class QuizInterface:
         self.window.config(background=THEME_COLOR)
         self.window.config(padx=20, pady=20)
 
-        self.score_label = Label(text=f"Score: 0", bg=THEME_COLOR, fg="white", font=("Arial", 13))
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white", font=("Arial", 13))
         self.score_label.grid(column=1, row=0)
 
         self.canvas = Canvas(width=300, height=250)
@@ -18,11 +18,11 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=80)
 
         right_button = PhotoImage(file="images/true.png")
-        self.correct_button = Button(image=right_button, highlightthickness=0)
+        self.correct_button = Button(image=right_button, highlightthickness=0, command=self.true_choice)
         self.correct_button.grid(row=2, column=0)
 
         wrong_button = PhotoImage(file="images/false.png")
-        self.incorrect_button = Button(image=wrong_button, highlightthickness=0)
+        self.incorrect_button = Button(image=wrong_button, highlightthickness=0,command=self.false_choice)
         self.incorrect_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -30,5 +30,30 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+            self.score_label.config(text=f"Score: {self.quiz.get_score()}")
+        else:
+            self.canvas.itemconfig(self.question_text, text=f"You have reached the end of the quiz. Your final score is {self.quiz.get_score()}")
+            self.correct_button.config(state="disabled")
+            self.incorrect_button.config(state="disabled")
+
+    def true_choice(self):
+        #is_right = self.quiz.check_answer("true")
+        self.give_feedback(self.quiz.check_answer("true"))
+
+
+    def false_choice(self):
+        #is_right = self.quiz.check_answer("false")
+        self.give_feedback(self.quiz.check_answer("false"))
+        #self.update_score()
+        #self.get_next_question()
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
