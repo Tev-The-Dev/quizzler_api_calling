9b3251caea465a2d20b8727121eba8f2c8a0a705
Tevyn <tevyn.payne@gmail.com>
2025-05-06 00:52:47 -0500

    Dynamically displaying the questions to the canvas

--- ui.py
+++ ui.py
@@ -1,9 +1,10 @@
 THEME_COLOR = "#375362"
 from tkinter import *
-import quiz_brain as qb
+from quiz_brain import QuizBrain
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(background=THEME_COLOR)
@@ -13,7 +14,7 @@ class QuizInterface:
         self.score_label.grid(column=1, row=0)
 
         self.canvas = Canvas(width=300, height=250)
-        self.question_text = self.canvas.create_text(150,125, text="placehoder", font=("Arial", 20, "italic"))
+        self.question_text = self.canvas.create_text(150,125, text="Placeholder Text", font=("Arial", 20, "italic"), width=290)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=80)
 
         right_button = PhotoImage(file="images/true.png")
@@ -24,4 +25,10 @@ class QuizInterface:
         self.incorrect_button = Button(image=wrong_button, highlightthickness=0)
         self.incorrect_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
